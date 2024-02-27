4a2fa39455ba11ab3edd97bd56b768c8b28121cd
jihoon_sung <jihoon_sung@tmax.co.kr>
2024-02-27 22:26:24 +0900

    feat: add chat
    - add chat form
    - add nickname form
    - add chat system

--- src/public/app.js
+++ src/public/app.js
@@ -1,19 +1,48 @@
 // const socket = new WebSocket("http://localhost:3000"); // Failed to construct 'WebSocket': The URL's scheme must be either 'ws' or 'wss'. 'http' is not allowed.
 const socket = new WebSocket(`ws://${window.location.host}`); // 서버로의 연결. server.js와의 socket이랑 의미가 다르다
 // 연결되면 서버로 "connection" event 전송
+const messageList = document.querySelector("ul");
+const msgForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
+
+//////////////////////////////////////////// utils
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
+////////////////////////////////////////////
 
 socket.addEventListener("open", () => {
   console.log("Connected to Server ✅");
 });
 
 socket.addEventListener("message", (msg) => {
-  console.log("[message] ", msg.data);
+  const li = document.createElement("li");
+  li.innerText = msg.data;
+  messageList.append(li);
 });
 
 socket.addEventListener("close", (msg) => {
   console.log("Disconnected from Server ❌");
 });
 
-setTimeout(() => {
-  socket.send("hello from the browser!");
-}, 3000);
+const handleNickSubmit = (event) => {
+  console.log("handleNickSubmit");
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+  input.value = "";
+};
+
+// TODO: 시스템 메시지 등을 위해 프론트에서도 parsing해야되는 등 작업이 많이 필요하다 -> 새로운 framework가 필요
+// TODO: 본인 빼고 다른 유저에게 보내고 싶으면?
+const handleMsgSubmit = (event) => {
+  console.log("handleMsgSubmit");
+  event.preventDefault(); // submit 이벤트가 발생하면 page reload되는걸 방지
+  const input = msgForm.querySelector("input");
+  socket.send(makeMessage("message", input.value));
+  input.value = "";
+};
+
+nickForm.addEventListener("submit", handleNickSubmit);
+msgForm.addEventListener("submit", handleMsgSubmit); // <form> 요소 자체에서 button 클릭 시 event fires.
--- src/server.js
+++ src/server.js
@@ -27,16 +27,39 @@ function onMessage(msg) {
 const server = http.createServer(app); // http 서버
 const wss = new WebSocket.Server({ server }); // webSocket 서버. 인자로 http server를 넣어서 둘 다 쓸 수 있도록 함.
 // 이렇게 함으로써 한 포트에 http & ws 둘 다 관리 가능
+const sockets = [];
 
 /**
+ * WebSocket.Server.on
  * @param {string} event connection / error / headers / listening / close
  * @param {*} socket  socket은 연결된 브라우저, 사람과 브라우저 사이와의 연결. WebSocket은 서버와 브라우저 사이의 연결.
  */
 wss.on("connection", (socket) => {
+  sockets.push(socket); // socket 연결 될 때마다 관리
+  socket["nickname"] = "Anonymous";
+
   console.log("Connected to Server  ✅"); // 누군가가 websocket 서버로 연결을 함.
-  socket.send("hello~");
+
+  /**
+   * WebSocket.on
+   * @param {string} event close / error / upgrade / message / open / ping|pong / unexpected-response
+   * @param {*} socket  socket은 연결된 브라우저, 사람과 브라우저 사이와의 연결. WebSocket은 서버와 브라우저 사이의 연결.
+   */
   socket.on("close", onSocketClose);
-  socket.on("message", onMessage);
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg); // string -> js object
+    switch (message.type) {
+      case "message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`[${socket.nickname}] ${message.payload}`)
+        ); // 모든 socket들에게 send
+        break;
+      case "nickname":
+        console.log(socket.nickname, "'s nick changed", message.payload);
+        socket["nickname"] = message.payload;
+        break;
+    }
+  });
 });
 
 // app.listen(3000, handleListen);
